61ac827bae1f1a8e44a1a95d54968ae9cbc362c0
johnbreau <johnbreau@github.com>
2016-09-17 13:20:16 -0500

    added main body component

---
+++ src/client/app/shared/main-body/main-body.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the navigation bar component.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'mp-main-body',
+  templateUrl: 'main-body.component.html',
+  styleUrls: ['main-body.component.css'],
+})
+
+export class MainBodyComponent {}
--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 
 import { ToolbarComponent } from './toolbar/index';
 import { NavbarComponent } from './navbar/index';
+import { MainBodyComponent } from './main-body/index';
 import { NameListService } from './name-list/index';
 
 /**
@@ -13,8 +14,8 @@ import { NameListService } from './name-list/index';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ToolbarComponent, NavbarComponent],
-  exports: [ToolbarComponent, NavbarComponent,
+  declarations: [ToolbarComponent, NavbarComponent, MainBodyComponent],
+  exports: [ToolbarComponent, NavbarComponent, MainBodyComponent,
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
